73c6c94757735ca035e4a89faca2c26dbb0481d7
Kevin Borrill <kevin.borrill@3sidedcube.com>
2019-03-11 23:01:11 +0000

    speak to gif bot

--- src/views/Request.vue
+++ src/views/Request.vue
@@ -4,7 +4,10 @@
       <div class="request-container" :style="loading ? loadingStyle : gifStyle">
         <div class="request-details" v-if="summary && gif">{{ summary }}</div>
       </div>
-      <input v-model="query" :disabled="loading" /><button v-on:click="doRequest" :disabled="loading">GIF me!</button>
+      <div class="request-actions">
+        <input v-model="query" :disabled="loading" /><button v-on:click="doRequest" :disabled="loading">GIF me!</button>
+        <button @click="beginRecording" v-if="recognition" :disabled="recording">{{ recording ? 'Speak!' : 'Listen' }}</button>
+      </div>
     </div>
     <div class="response">
       <pre>
@@ -19,9 +22,11 @@
 // @ is an alias to /src
 import axios from 'axios'
 import loadingGif from '../assets/loading.gif'
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+
 const witConfig = {
   headers: {
-    Authorization: `Bearer ${process.env.VUE_APP_WIT_CLIENT_TOKEN}`,
+    Authorization: `Bearer ${process.env.VUE_APP_WIT_CLIENT_TOKEN}`
   }
 }
 export default {
@@ -36,11 +41,14 @@ export default {
       gif: null,
       response: null,
       loadingStyle: { 'background-image': `url(${loadingGif})`, 'max-width': `400px`, 'height': `300px` },
-      gifStyle: null
+      gifStyle: null,
+      recognition: SpeechRecognition ? new SpeechRecognition() : null,
+      recording: false
     }
   },
   methods: {
     async doRequest () {
+      this.recording = false
       try {
         this.loading = true
         this.response = null
@@ -72,6 +80,17 @@ export default {
       this.gif = this.response.data.giphy
       this.summary = `${this.response.data.location.name} - ${this.response.data.weather.summary}`
     },
+    beginRecording () {
+      this.recognition.lang = 'en-GB'
+      this.recognition.interimResults = true
+      this.recognition.start()
+      this.recording = true
+    },
+    recognitionRecieved (e) {
+      let last = e.results.length - 1
+      let text = e.results[last][0].transcript
+      this.query = text
+    },
     doUnknown  () {
       console.log('Something broke...', this.witResponse)
       this.gif = {
@@ -84,6 +103,10 @@ export default {
   },
   mounted () {
     this.doRequest()
+    if (this.recognition) {
+      this.recognition.onresult = this.recognitionRecieved
+      this.recognition.onspeechend = this.doRequest
+    }
   }
 }
 </script>
@@ -101,11 +124,16 @@ export default {
     align-items: center;
     width: 100%;
     margin: 8px;
-    input {
+    .request-actions {
       margin-top: 20px;
-      width: 50%;
+      width: 80%;
+      display: flex;
+      flex-wrap: wrap;
+      input {
+        flex-grow: 1;
+      }
       @media (max-width: 500px) {
-        width: 80%;
+        width: 100%;
       }
     }
     .request-container {
